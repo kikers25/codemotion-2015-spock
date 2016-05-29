56fc5092503a70bf92cdf5e50c99ed2c2539c01e
Martin Enrique <kikers25@gmail.com>
2016-05-29 19:58:37 +0200

    Added E2 test in Java

---
+++ src/test/java/E2_PowerAsserts.java
@@ -0,0 +1,44 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Ignore("Comment this annotation to check the errors")
+public class E2_PowerAsserts {
+
+    @Test
+    public void should_fail_with_string() {
+        assertThat(reverse("Hello Codemotion!"), is("ointmodeoC olleH"));
+    }
+
+    @Test
+    public void should_fail_with_maps_and_lists() {
+        // given:
+        Map<String, Object> child1 =Collections.unmodifiableMap(new HashMap<String, Object>(){{
+            put("name", "Judith");
+            put("age", 8);
+        }});
+        Map<String, Object> child2 =new HashMap<String, Object>(){{
+            put("name", "Adriana");
+            put("age", 5);
+        }};
+        Map<String, Object> data =new HashMap<String, Object>(){{
+            put("name", "Iván");
+            put("age", 35);
+            put("childs", Arrays.asList(child1, child2));
+        }};
+
+        // expect:
+        assertThat(((List<Map<String, Object>>) data.get("childs")).get(0).get("name"), is("Adriana"));
+    }
+
+    private String reverse(String myString) {
+        StringBuilder myStringBuilder = new StringBuilder(myString);
+
+        return myStringBuilder.reverse().toString();
+    }
+
+}
