3af9356a4cfa97e3e8f3ffe34ee6fdbc9e1f5b9f
Martin Enrique <kikers25@gmail.com>
2016-05-30 23:02:25 +0200

    Added E3 test in Java

---
+++ src/test/java/codemotion2015/E3_GroovyJ.java
@@ -0,0 +1,58 @@
+package codemotion2015;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+
+public class E3_GroovyJ {
+
+        @Test public void should_add_an_element_to_a_list() {
+                List<Integer> numbers = DataHelper.someFibonacciNumbers();
+
+                numbers.add(21);
+
+                assertThat(numbers, hasItem(21));
+        }
+
+        @Test public void should_make_some_assertions_in_elements_in_a_list() {
+                List<Person> persons = DataHelper.makePersonList();
+
+                assertThat(persons, hasSize(4));
+                assertThat(persons.stream()
+                    .map(Person::getName)
+                    .collect(Collectors.toList()), containsInAnyOrder("Sheldon", "Leonard", "Rajesh", "Howard"));
+                assertThat(persons.stream()
+                    .map(Person::getName)
+                    .sorted()
+                    .collect(Collectors.toList()), containsInAnyOrder("Howard", "Leonard", "Rajesh", "Sheldon"));
+                assertThat(persons.stream()
+                    .map(p -> p.getLastName().length())
+                    .collect(Collectors.toList()), contains(6, 10, 12, 8));
+                // persons.name.min { it.length() } == 'Rajesh'
+        }
+
+        @Test public void should_remove_an_element_from_a_map() {
+                // given:
+                Map<String, ?> map = DataHelper.makePersonMap();
+
+                // when:
+                map.remove("name");
+
+                assertThat(map.size(), is(2));
+                assertThat(map.keySet(), not(contains("name")));
+                // map.lastName == 'López'
+                assertThat(map.get("lastName"), is("López"));
+                // map['lastName'] == 'López'
+                assertThat(map.get("age"), is (35));
+        }
+}
