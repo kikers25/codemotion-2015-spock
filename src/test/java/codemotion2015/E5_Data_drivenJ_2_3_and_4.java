71ccb434f7f24b953697deea1f11e77d949668fa
Martin Enrique <kikers25@gmail.com>
2016-06-01 21:26:29 +0200

    Added second, third and fourth tests of E5 test in Java. Unroll and data table are not available with Junit

---
+++ src/test/java/codemotion2015/E5_Data_drivenJ_2_3_and_4.java
@@ -0,0 +1,45 @@
+package codemotion2015;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Parameterized.class)
+public class E5_Data_drivenJ_2_3_and_4 {
+
+        @Parameterized.Parameter(value = 0)
+        public Integer x;
+
+        @Parameterized.Parameter(value = 1)
+        public Integer y;
+
+        @Parameterized.Parameter(value = 2)
+        public Integer result;
+
+        @Parameterized.Parameters
+        public static Collection<Integer[]> data() {
+                return Arrays.asList(
+                    new Integer[]{1, 2 , 1},
+                    new Integer[]{-1, -2, -2},
+                    new Integer[]{0, 1, 0});
+        }
+
+        @Test public void should_get_the_minimum() {
+                assertThat(Math.min(x, y), is(result));
+        }
+
+        /* data tables like:
+            x  | y  | result
+            1  | 2  | 1
+            -1 | -2 | -2
+            0  | 1  | 0
+
+            and unroll are not available in Junit
+        * */
+}
