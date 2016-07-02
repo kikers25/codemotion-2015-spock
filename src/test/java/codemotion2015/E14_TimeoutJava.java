43fb8a9d2edf05708ad83d27d4709186f4d46ff3
Enrique <kikers25@gmail.com>
2016-07-02 19:19:38 +0200

    added test E14 in Java

---
+++ src/test/java/codemotion2015/E14_TimeoutJava.java
@@ -0,0 +1,18 @@
+package codemotion2015;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+@Ignore("Remove to check that the test fails")
+public class E14_TimeoutJava {
+
+        @Test(timeout = 500) public void should_fail_after_500_milliseconds() throws InterruptedException {
+                //given:
+                Thread.sleep(500);
+
+                //expect:
+                assertTrue(true);
+        }
+}
