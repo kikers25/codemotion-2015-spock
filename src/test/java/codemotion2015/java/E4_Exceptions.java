f6f83aff3e1e9d250d461df603d0fd2672131111
Enrique <kikers25@gmail.com>
2016-07-02 19:32:03 +0200

    I forgot to rename one Java class

--- src/test/java/codemotion2015/java/E4_Exceptions.java
+++ src/test/java/codemotion2015/java/E4_Exceptions.java
@@ -2,7 +2,7 @@ package codemotion2015.java;
 
 import org.junit.Test;
 
-public class E4_ExceptionsJ {
+public class E4_Exceptions {
 
         @Test(expected = NumberFormatException.class)
         public void should_throw_an_exception() {
