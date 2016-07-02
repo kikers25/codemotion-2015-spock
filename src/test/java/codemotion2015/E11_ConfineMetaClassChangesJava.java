c3ae451ce9f4adc94892e7ea03e3a8a23b024fbd
Enrique <kikers25@gmail.com>
2016-07-02 18:27:50 +0200

    added tests E11 for Java

---
+++ src/test/java/codemotion2015/E11_ConfineMetaClassChangesJava.java
@@ -0,0 +1,7 @@
+package codemotion2015;
+
+public class E11_ConfineMetaClassChangesJava {
+
+        // It is not possible to do this. Java has only one Metaclass Class.
+        // String is a final class. We cannot extend it.
+}
