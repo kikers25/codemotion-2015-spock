d18ef2a03fc3c43bda6895598689f13ac1645079
Enrique <kikers25@gmail.com>
2016-07-02 19:14:42 +0200

    added test E13 in Java

---
+++ src/test/java/codemotion2015/E13_Requires_IgnoreIfJava.java
@@ -0,0 +1,50 @@
+package codemotion2015;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assume.assumeThat;
+
+public class E13_Requires_IgnoreIfJava {
+
+        public static final String OS = System.getProperty("os.name").toLowerCase();
+
+        @Test public void should_only_run_on_Linux() {
+                assumeThat(OS, anyOf(
+                    containsString("nix"),
+                    containsString("nux"),
+                    containsString("aix")));
+                assertTrue(true);
+        }
+
+        @Test public void should_only_run_on_Windows() {
+                assumeThat(OS, containsString("windows"));
+                assertTrue(true);
+        }
+
+        @Test public void should_be_ignore_in_Java8() {
+                // Similar but not the same than the groovy version
+                assumeThat(System.getProperty("java.version"), not(startsWith("1.8.")));
+                assertTrue(false);
+        }
+
+        @Test public void should_be_ignore_in_Java9() {
+                // Similar but not the same than the groovy version
+                assumeThat(System.getProperty("java.version"), not(startsWith("1.9.")));
+                assertTrue(true);
+        }
+
+        @Test public void should_run_for_Groovy_conference() {
+                assumeThat(groovyConferences(), hasItem("Greach"));
+                assertTrue(true);
+        }
+
+        private static List<String> groovyConferences() {
+                return asList("Greach", "GR8Conf", "GGX");
+        }
+
+}
