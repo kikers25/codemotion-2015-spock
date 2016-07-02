43dead82994372bacf348b63dd7b84d8f8102238
Enrique <kikers25@gmail.com>
2016-07-02 18:18:17 +0200

    added tests E10

---
+++ src/test/java/codemotion2015/E10_OldJava.java
@@ -0,0 +1,26 @@
+package codemotion2015;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class E10_OldJava {
+
+        @Test public void should_should_increase_the_size_of_the_list_when_adding_a_number() {
+                //given:
+                // Arrays.asList generates a fixed list and when a value is added it will throw an exception
+                List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 5, 8, 13));
+                int old = numbers.size();
+
+                //when
+                numbers.add(21);
+
+                //then
+                assertThat(numbers.size(), is(old + 1));
+        }
+}
